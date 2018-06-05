497bf6c40986a64047d7b8a9aeed6e6a7115e192
Komstuch <komstuchxbox@gmail.com>
2018-06-05 21:53:13 +0200

    Alpha version
    Level progression added - TODO - difficultu setting

--- Assets/Scripts/Button.cs
+++ Assets/Scripts/Button.cs
@@ -1,21 +1,35 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 
 public class Button : MonoBehaviour {
 
 	public GameObject defenderPrefab;
-
-	private Button[] buttonArray;
 	public static GameObject selectedDefender;
+	
+	private Button[] buttonArray;
+	private Text costText;
 
 	// Use this for initialization
 	void Start () {
 		buttonArray = GameObject.FindObjectsOfType<Button>();
+		InitializeCost ();
+		
+	}
 
+	void InitializeCost ()
+	{
+		costText = GetComponentInChildren<Text> ();
+		if (!costText) {
+			Debug.LogWarning (name + " has no cost text attached");
+		}
+		else {
+			costText.text = defenderPrefab.GetComponent<Defender> ().starCost.ToString ();
+		}
 	}
 	
 	// Update is called once per frame
-	void Update () {
+	void Update () {	
 	
 	}
 	
