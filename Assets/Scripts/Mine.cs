11ef00a20ece68fbd0cdab1db3143f47d4a6a6f9
Komstuch <komstuchxbox@gmail.com>
2018-06-07 17:36:36 +0200

    Refine Mine.cs code - Raibow now deals damage to all ememies which are colliding with it

--- Assets/Scripts/Mine.cs
+++ Assets/Scripts/Mine.cs
@@ -6,42 +6,48 @@ public class Mine : MonoBehaviour {
 
     private Animator animator;
     private GameObject currentTarget;
+    private List<GameObject> Targets = new List<GameObject>(); // List of tagets which are currently colliding with the mine
 
 	void Start () {
         animator = GetComponent<Animator>();
 	}
 	
 	void Update () {
-		
-	}
-
-    //Set the attacking flag as colliders enter
-    void OnTriggerEnter2D(Collider2D collision){
-        if (collision.GetComponent<Attacker>()) {
+        if (Targets.Count > 0) {
             animator.SetBool("isUnderEnemy", true);
-            DamageOverTime(collision.gameObject);
         }
-    }
-
-    void OnTriggerExit2D(Collider2D collision)
-    {
-        if (collision.GetComponent<Attacker>()){
+        else {
             animator.SetBool("isUnderEnemy", false);
         }
-    }
+	}
 
-    void StrikeCurrentAttacker(float damage){
-        if (currentTarget){
-            Health health = currentTarget.GetComponent<Health>();
-            if (health){
-                health.DealDamage(damage);
-            }
+    //Add Enemy to Targets list as they enter the Raibow Collider
+    void OnTriggerEnter2D(Collider2D collision){
+
+        if (collision.gameObject.GetComponent<Attacker>()) {
+            Targets.Add(collision.gameObject);
         }
     }
 
-    void DamageOverTime(GameObject obj){
-        currentTarget = obj;
+    //Remove Enemy from Targets list as they exit the Raibow Collider
+    void OnTriggerExit2D(Collider2D collision){
+        if (collision.gameObject.GetComponent<Attacker>())
+        {
+            Targets.Remove(collision.gameObject);
+        }
+    }
 
+    //Deal damage to all attackers on the "Targets" list - method called from the animator
+    void StrikeAllAttackers(float damage){
+        foreach (GameObject currentTarget in Targets) {
+            
+            Health health = currentTarget.GetComponent<Health>();
+            if (health)
+            {
+                health.DealDamage(damage);
+                print("Dealt: " + damage +" to: " + currentTarget.name);
+            }
+        }
     }
 
 }
